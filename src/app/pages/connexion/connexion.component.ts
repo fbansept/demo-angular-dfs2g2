680345127b69634552b450b020fe1c2d5490f930
fbansept <bansept.franck@gmail.com>
2024-11-26 10:54:47 +0100

    connexion

--- src/app/pages/connexion/connexion.component.ts
+++ src/app/pages/connexion/connexion.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-connexion',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.scss'
 })
 export class ConnexionComponent {
+
+  formBuilder = inject(FormBuilder)
+
+  formulaire = this.formBuilder.group(
+    {
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]]
+    }
+  )
+
+  onConnexion() {
+
+    if (this.formulaire.valid) {
+
+      console.log(this.formulaire.value)
+
+    }
+
+  }
+
 
 }
